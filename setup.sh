6be75c1b8e844b3e6c2917fcdd9bb742b008f0c7
ofischbach24 <139179568+ofischbach24@users.noreply.github.com>
2024-02-27 09:39:18 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ gem install evdev pi_piper
 chmod +x app.rb
 
 # Create the controller monitoring shell script
-echo -e "#!/bin/bash\n\nwhile true; do\n  controller_event=\$(ls /dev/input/ | grep \"event.*\" | grep -o \"event[0-9]*\" | head -n 1)\n\n  if [ -n \"\$controller_event\" ]; then\n    echo \"PS4 controller detected on /dev/input/\$controller_event\"\n    ./your_script_name.rb \"/dev/input/\$controller_event\"\n    sleep 1  # Adjust sleep time as needed to avoid rapid re-checking\n  else\n    sleep 1\n  fi\ndone" > controller_monitor.sh
+echo -e "#!/bin/bash\n\nwhile true; do\n  controller_event=\$(ls /dev/input/ | grep \"event.*\" | grep -o \"event[0-9]*\" | head -n 1)\n\n  if [ -n \"\$controller_event\" ]; then\n    echo \"PS4 controller detected on /dev/input/\$controller_event\"\n    ./app.rb \"/dev/input/\$controller_event\"\n    sleep 1  # Adjust sleep time as needed to avoid rapid re-checking\n  else\n    sleep 1\n  fi\ndone" > controller_monitor.sh
 
 # Make the controller monitoring script executable
 chmod +x controller_monitor.sh
